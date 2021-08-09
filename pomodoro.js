04a1db0823881b38d8244d8021cd3e7eb410c097
pedromchd <pedromchd01@outlook.com>
2021-08-09 13:55:00 -0300

    meu deus vo morr

--- pomodoro.js
+++ pomodoro.js
@@ -1,19 +1,19 @@
 const qSel = document.querySelectorAll.bind(document);
 const time = qSel("#timer")[0];
-const btn1 = qSel("button")[0];
-const btn2 = qSel("button")[1];
+const btns = qSel("button");
+const icon = qSel("i");
 let counter;
 
-btn1.addEventListener('click', () => {
+btns[0].addEventListener('click', () => {
+  toggle(0, 1);
   clearInterval(counter);
-  const count = count2(25);
-  counter = interval(count);
+  counter = interval(count2(25));
 });
 
-btn2.addEventListener('click', () => {
+btns[1].addEventListener('click', () => {
+  toggle(1, 0);
   clearInterval(counter);
-  const count = count2(5);
-  counter = interval(count);
+  counter = interval(count2(5));
 });
 
 function count2(t) {
@@ -29,13 +29,29 @@ function timer(mil) {
   mil -= min * 60000;
   let sec = parseInt(mil / 1000);
 //mil -= sec * 1000;
-  min = (min < 10) ? `0${min}` : min;
-  sec = (sec < 10) ? `0${sec}` : sec;
-  return `${min}:${sec}`;
+  return getTime(min, sec);
+}
+
+function getTime(...hms) {
+  hms.forEach((v, i) => {
+    hms[i] = (v < 10) ? `0${v}` : v;
+  });
+  return hms.join(':');
 }
 
 function interval(to) {
   return setInterval(() => {
     time.innerText = timer(left(to));
   }, 50);
+}
+
+function onOff(k) {
+  btns[k].disabled = !(btns[k].disabled);
+  icon[k].classList.toggle("fa-spinner");
+  icon[k].classList.toggle("fa-pulse");
+}
+
+function toggle(i, j) {
+  onOff(i);
+  if (btns[j].disabled) onOff(j);
 }
