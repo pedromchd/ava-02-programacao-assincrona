b24ad92c3832ded34c52b241ef9da08d41ac0f8c
pedromchd <pedromchd01@outlook.com>
2021-08-28 18:09:34 -0300

    interações de busca

--- palavras-worker.js
+++ palavras-worker.js
@@ -1,6 +1,9 @@
 onmessage = msg => {
   let val = msg.data[0];
   let ind = msg.data[1];
+  setTimeout(() => {
+    postMessage(val);
+  }, 2000);
   // let txt = (ind) ? formedBy(val) : hasThe(val);
   // postMessage(txt);
 };
--- palavras.js
+++ palavras.js
@@ -4,14 +4,22 @@ const button = formEl.querySelectorAll('button');
 const output = document.querySelector('span.navbar-text');
 
 const worker = new Worker('palavras-worker.js');
+const alarme = new Audio('docs/alloy.mp3');
+alarme.volume = 0.1;
+
+const loader = document.querySelector('i.fa-spinner');
+const toggle = ele => ele.hidden = (ele.hidden) ? ele.hidden = false : ele.hidden = true;
 
 formEl.onsubmit = evt => evt.preventDefault();
 
 button.forEach((btn, ind) => {
   btn.addEventListener('click', () => {
     let val = input_.value;
+    toggle(loader);
     output.innerText = 'Buscando...';
     worker.onmessage = msg => {
+      toggle(loader);
+      alarme.play();
       output.innerText = msg.data;
     }
     worker.postMessage([val, ind]);
