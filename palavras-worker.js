1e5c8349aea139cd3f8a08e2f743cf41e4902b5a
pedromchd <pedromchd01@outlook.com>
2021-08-28 17:13:47 -0300

    js and worker presetup

---
+++ palavras-worker.js
@@ -0,0 +1,13 @@
+onmessage = msg => {
+  let val = msg.data[0];
+  let ind = msg.data[1];
+  // let txt = (ind) ? formedBy(val) : hasThe(val);
+  // postMessage(txt);
+};
+
+// function getDataBase() {
+
+// }
+
+// function formedBy() {}
+// function hasThe() {}
--- palavras.js
+++ palavras.js
@@ -1,12 +1,19 @@
-const button = document.querySelectorAll('button');
-const inpute = document.querySelector('input');
-const result = document.querySelector('.navbar-text');
+const formEl = document.querySelector('form');
+const input_ = formEl.querySelector('input');
+const button = formEl.querySelectorAll('button');
+const output = document.querySelector('span.navbar-text');
+
 const worker = new Worker('palavras-worker.js');
+
+formEl.onsubmit = evt => evt.preventDefault();
 
 button.forEach((btn, ind) => {
   btn.addEventListener('click', () => {
-    let val = inpute.value;
-    result.innerText = val;
-    // worker.onmessage(ide);
+    let val = input_.value;
+    output.innerText = 'Buscando...';
+    worker.onmessage = msg => {
+      output.innerText = msg.data;
+    }
+    worker.postMessage([val, ind]);
   });
 });
