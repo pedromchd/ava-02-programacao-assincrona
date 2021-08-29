607f4e8312c75d84c1c2b364bc5806515c5e5dbe
pedromchd <pedromchd01@outlook.com>
2021-08-29 15:05:55 -0300

    fetch concluido, desenvolvendo as functions

--- palavras-finder.js
+++ palavras-finder.js
@@ -1,18 +1,31 @@
-onmessage = msg => {
-  let val = msg.data[0];
-  let ind = msg.data[1];
-  setTimeout(() => {
-    postMessage(val);
-  }, 2000);
-  // let txt = (ind) ? formedBy(val) : hasThe(val);
-  // postMessage(txt);
+onmessage = async msg => {
+  const val = msg.data[0];
+  const ind = msg.data[1];
+  const raw = await fetch('docs/palavras.txt');
+  const txt = await raw.text();
+  const get = txt.split(/\r\n/);
+  const res = (ind) ? formedBy(val, get) : hasThe(val, get);
+  postMessage(res);
 };
 
-// function getDataBase() {
+//sistema de contagem
 
-// }
+function hasThe(value, words) {
+  const found = Array();
+  words.forEach(w => {
+    try {
+      value.split('').forEach(l => {
+        if (w.toLowerCase().indexOf(l) < 0) throw 'aaa';
+      });
+      found.push(w);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  });
+  return found.join(', ');
+}
 
-// function formedBy() {}
-// function hasThe() {}
-
-// tratar o fech - sistema de contagem
+function formedBy(value, words) {
+  return words[1];
+}
